fcb376b1c2c8a6a5da622dbec8285d7768b0c846
hcj <chiajen870907@gmail.com>
2022-02-14 15:37:37 +0800

    first commit

---
+++ Shopee/QX_Shopee_Checkin.js
@@ -0,0 +1,28 @@
+const shopeeUrl = {
+    url: 'https://shopee.tw/mkt/coins/api/v2/checkin',
+    method: "POST",
+    headers: {
+        'Cookie': $prefs.valueForKey("CookieSP") + ';SPC_EC=' + $prefs.valueForKey("SPC_EC") + ';',
+        'X-CSRFToken': $prefs.valueForKey("CSRFTokenSP"),
+    }
+};
+
+$task.fetch(shopeeUrl).then(response => {
+    if (response.statusCode == 200) {
+        let obj = JSON.parse(response.body);
+        if (obj["data"]["success"]) {
+            var user = obj["data"]["username"];
+            var coins = obj["data"]["increase_coins"];
+            var checkinday = obj["data"]["check_in_day"];
+            $notify("蝦皮 " + user + " 已連續簽到 " + checkinday + " 天", "", "今日已領取 " + coins + "💰💰💰");
+            $done();
+        }
+        $done();
+    } else {
+        $notify("🍤 蝦皮 Cookie 已過期或網路錯誤‼️", "", "請重新更新 Cookie 重試 🔓");
+        $done();
+    }
+}, reason => {
+    $notify("蝦皮簽到", "", "連線錯誤‼️")
+    $done();
+});
---
+++ Shopee/QX_Shopee_Luckydraw.js
@@ -0,0 +1,39 @@
+var shopeeluckydrawUrl = {
+    url: 'https://games.shopee.tw/luckydraw/api/v1/lucky/event/a3267155f3ec89c2',
+    method: "POST",
+    headers: {
+        'Cookie': $prefs.valueForKey("CookieSP") + ';SPC_EC=' + $prefs.valueForKey("SPC_EC") + ';',
+        'X-CSRFToken': $prefs.valueForKey("CSRFTokenSP"),
+    },
+    body: {
+        request_id: (Math.random() * 10 ** 20).toFixed(0).substring(0, 16),
+        app_id: "E9VFyxwmtgjnCR8uhL",
+        activity_code: "e37b7dec5976a29c",
+        source: 0,
+      },
+};
+
+$task.fetch(shopeeUrl).then(response => {
+    if (response.statusCode == 200) {
+        let obj = JSON.parse(response.body);
+        
+        if(obj["msg"] == "no chance"){
+            $notify("🍤 今日已領過蝦幣寶箱", "", "每日只能領一次‼️");
+            $done();
+        }else if(obj["msg" == "success"]){
+            var packagename = obj["data"]["package_name"];
+            $notify("🍤 蝦幣寶箱領取成功 ✅", "", "獲得 👉 " + packagename + " 💰");
+            $done();
+        }else if (obj["msg"] == "expired" || obj["msg"] == "event already end") {
+            $notify("🍤 蝦幣寶箱活動已過期 ❌", "", "請嘗試更新模組或腳本，或等待作者更新‼️");
+            $done();
+        }
+        $done();
+    } else {
+        $notify("🍤 蝦皮 Cookie 已過期‼️", "", "請重新更新 Cookie 重試 🔓");
+        $done();
+    }
+}, reason => {
+    $notify("🍤 蝦幣寶箱", "", "連線錯誤‼️")
+    $done();
+});
