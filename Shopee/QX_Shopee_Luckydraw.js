87c52e68cae82582080107c3d1641fbe15ffec14
HCJ <chiajen870907@gmail.com>
2022-02-22 00:00:51 +0800

    fix

--- Shopee/QX_Shopee_Luckydraw.js
+++ Shopee/QX_Shopee_Luckydraw.js
@@ -25,4 +25,48 @@ let luckyRrawRequest = {
 };
 console.log("flag2");
 
+$task.fetch(luckyRrawGetIdRequest).then(response => {
+	console.log(response);
+	if (response.statusCode == 200) {
+		const obj = JSON.parse(data);
+		if (obj['msg'] !== 'success') {
+			$notify('🍤 蝦幣寶箱網址查詢', '', '未知錯誤，請稍候再手動嘗試‼️');
+			$done();
+		}else{
+			const eventUrl = obj['data']['basic']['event_code'];
+			luckyRrawRequest.url = 'https://games.shopee.tw/luckydraw/api/v1/lucky/event/' + eventUrl;
+			console.log('🍤 蝦幣寶箱新網址獲取成功： ' + luckyRrawRequest.url);
+			// 開寶箱
+			$task.fetch(luckyRrawRequest).then(response => {
+				if (response.statusCode == 200) {
+					const obj = JSON.parse(data);
+					if (obj['msg'] == 'no chance') {
+						$notify('🍤 今日已領過蝦幣寶箱', '', '每日只能領一次‼️');
+						$done();
+					} else if (obj['msg'] == 'success') {
+						const packagename = obj['data']['package_name'];
+						$notify('🍤 蝦幣寶箱領取成功 ✅', '', '獲得 👉 ' + packagename + ' 💎');
+						$done();
+					} else if (obj['msg'] == 'expired' || obj['msg'] == 'event already end') {
+						$notify('🍤 蝦幣寶箱活動已過期 ❌', '', '請嘗試更新模組或腳本，或等待作者更新‼️');
+						$done();
+					}
+				} else {
+					$notify('🍤 蝦皮 Cookie 已過期‼️', '', '請重新抓取 🔓');
+					$done();
+				}
+			}, reason => {
+				$notify('🍤 蝦幣寶箱', '', '連線錯誤‼️'));
+				$done();
+			});
+		} 
+	}else{
+		$notify('🍤 蝦皮 Cookie 已過期‼️', '', '請重新抓取 🔓');
+		$done();
+	}
 
+	
+}, reason => {
+    $notify("🍤 蝦幣寶箱網址查詢", "", "連線錯誤‼️")
+    $done();
+});
