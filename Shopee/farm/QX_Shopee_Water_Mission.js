f601f96020c9d9d35abddf453c4c9e067415c8cf
hcj <chiajen870907@gmail.com>
2022-03-02 18:15:03 +0800

    add

---
+++ Shopee/farm/QX_Shopee_Water_Mission.js
@@ -0,0 +1,98 @@
+let request = {
+  url: '',
+  headers: {
+    'Cookie': $prefs.valueForKey('CookieSP') + ';SPC_EC=' + $prefs.valueForKey('SPC_EC') + ';',
+    'X-CSRFToken': $prefs.valueForKey('CSRFTokenSP'),
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    actionKey: ''
+  }),
+};
+
+let missions = [
+  {
+    actionKey: 'act_play_bubble_game',
+    missionName: '🍤 玩蝦皮泡泡王任務'
+  },
+  {
+    actionKey: 'act_play_candy_game',
+    missionName: '🍤 玩蝦皮消消樂任務'
+  }
+];
+
+for (let i = 0; i < 10; i++) {
+  missions.push({
+    actionKey: 'act_Receive_Water',
+    missionName: '🍤 收到站內朋友助水任務'
+  });
+}
+
+for (let i = 0; i < 10; i++) {
+  missions.push({
+    actionKey: 'act_Help_Watering',
+    missionName: '🍤 幫站內朋友澆水任務'
+  });
+}
+
+function waterMission(index) {
+  sleep(0.5);
+  const now = new Date().getTime();
+  const missionName = missions[index].missionName;
+  const actionKey = missions[index].actionKey;
+  request.url = url = 'https://games.shopee.tw/farm/api/task/action?t=' + now;
+  request.body.actionKey = actionKey;
+
+  $task.fetch(request).then(response => {
+    if (response.status === 200) {
+      try {
+        const obj = JSON.parse(data);
+        if (obj.msg === 'success') {
+          console.log(missions[index].missionName + '成功 ✅');
+          $notify('🍤 蝦皮水滴任務', 
+            missions[index].missionName, 
+            '任務成功 ✅',
+          );
+        } else if (obj.msg === 'lock failed.') {
+          $notify('🍤 蝦皮水滴任務錯誤',
+            missionName,
+            '連線請求過於頻繁',
+          );
+        } else {
+          $notify('🍤 蝦皮水滴任務錯誤',
+            missionName + '錯誤',
+            obj.msg
+          );
+        }
+      } catch (error) {
+        $notify('🍤 蝦皮水滴任務錯誤',
+          missionName + '錯誤',
+          error
+        );
+      }
+    } else {
+      $notify('🍤 蝦皮 Cookie 已過期或網路錯誤‼️',
+        '',
+        '請重新更新 Cookie 重試 🔓'
+      );
+    }
+
+    if (index < missions.length - 1) {
+      waterMission(index + 1);
+    }else {
+      $notify('🍤 蝦皮水滴任務完成 ✅', '', '');
+    }
+    $done();
+  }, reason => {
+    $notify("🍤 蝦皮果園水滴任務打卡", "", "連線錯誤‼️");
+    $done();
+  });
+
+}
+
+function sleep(seconds) {
+  const waitUntil = new Date().getTime() + seconds * 1000;
+  while (new Date().getTime() < waitUntil) true;
+}
+
+waterMission(0);
---
+++ Shopee/farm/QX_Shopee_Water_Signin_Bundle.js
@@ -0,0 +1,107 @@
+const getSignInBundleListRequest = {
+  url: 'https://games.shopee.tw/farm/api/sign_in_bundle/list?t=' + new Date().getTime(),
+  method:'GET',
+  headers: {
+    'Cookie': $prefs.valueForKey('CookieSP') + ';SPC_EC=' + $prefs.valueForKey('SPC_EC') + ';',
+    'X-CSRFToken': $prefs.valueForKey('CSRFTokenSP'),
+    'Content-Type': 'application/json',
+  },
+};
+
+let claimSignInBundleRequest = {
+  url: 'https://games.shopee.tw/farm/api/sign_in_bundle/claim?t=' + new Date().getTime(),
+  metohs:'POST',
+  headers: {
+    'Cookie': $prefs.valueForKey('CookieSP') + ';SPC_EC=' + $prefs.valueForKey('SPC_EC') + ';',
+    'X-CSRFToken': $prefs.valueForKey('CSRFTokenSP'),
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    'day': 0,
+    'forceClaim': true
+  })
+};
+
+function getSignInBundleList() {
+
+  $task.fetch(getSignInBundleListRequest).then(response => {
+    if (response.status === 200) {
+      try {
+        const obj = JSON.parse(data);
+        const day = obj.data.day;
+        const claimed = obj.data.signInBundlePrizes[day - 1].claimed;
+        if (claimed) {
+          console.log('🍤 蝦蝦果園今日已簽到‼️');
+          // $notify('🍤 蝦蝦果園今日簽到獎勵',
+          //   '',
+          //   '今日已簽到‼️'
+          // );
+          $done();
+          return;
+        }
+        claimSignInBundleRequest.body.day = day;
+        claimSignInBundle(day);
+      } catch (error) {
+        $notify('🍤 蝦蝦果園獲得今日簽到獎勵列表',
+          '',
+          '發生錯誤‼️' + error
+        );
+        $done();
+      }
+    } else {
+      $notify('🍤 蝦皮 Cookie 已過期或網路錯誤‼️',
+        '',
+        '請重新更新 Cookie 重試 🔓'
+      );
+      $done();
+    }
+    $done();
+  }, reason => {
+    $notify("🍤 蝦皮果園水滴任務打卡", "", "連線錯誤‼️");
+    $done();
+  });
+
+}
+
+function claimSignInBundle() {
+  $task.fetch(claimSignInBundleRequest).then(response => {
+    if (response.status === 200) {
+      try {
+        const obj = JSON.parse(data);
+        if (obj.msg === 'success') {
+          const day = obj.data.day;
+          const prize = obj.data.signInBundlePrizes[day - 1];
+          let prizeName = '';
+          if (prize.prizeDetail)
+          {
+            prizeName = prize.prizeDetail.prizeName;
+          }
+          else {
+            prizeName = prize.prizeNum + ' 滴水 💧';
+          }
+          $notify('🍤 蝦蝦果園今日簽到成功 ✅',
+          '',
+          '獲得 ' + prizeName
+        );
+        }
+      } catch (error) {
+        $notify('🍤 蝦蝦果園今日簽到獎勵',
+          '',
+          '獲得獎勵錯誤‼️' + error
+        );
+      }
+    } else {
+      $notify('🍤 蝦皮 Cookie 已過期或網路錯誤‼️',
+        '',
+        '請重新更新 Cookie 重試 🔓'
+      );
+    }
+    $done();
+  }, reason => {
+    $notify("🍤 蝦皮果園水滴任務打卡", "", "連線錯誤‼️");
+    $done();
+  });
+
+}
+
+getSignInBundleList();
